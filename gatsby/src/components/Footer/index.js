0ce5888fed8a854b47367fdcc1638f43223eabb0
PaoloDuzioni <paolo.duzioni@gmail.com>
2020-12-28 21:02:06 +0100

    refactoring: footer copy and added Logo

--- gatsby/src/components/Footer/index.js
+++ gatsby/src/components/Footer/index.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import { graphql, useStaticQuery } from 'gatsby';
+import Logo from '../Logo';
 import styles from './style.module.scss';
 
 const Footer = () => {
+    // Get site data
+    const { site } = useStaticQuery(graphql`
+        query {
+            site {
+                siteMetadata {
+                    title
+                    description
+                }
+            }
+        }
+    `);
+
     return (
-        <footer className={`text-center ${styles.footer}`}>
-            <p>&copy; Paolo's Pizza {new Date().getFullYear()}</p>
+        <footer className={`mt-2 text-center ${styles.footer}`}>
+            <Logo logoWidth="80" />
+            <p>
+                &copy; {site.siteMetadata.title} {new Date().getFullYear()} -{' '}
+                {site.siteMetadata.description}
+            </p>
         </footer>
     );
 };
--- gatsby/src/pages/404.js
+++ gatsby/src/pages/404.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import SEO from '../components/SEO';
 
 const FourOhFourPage = () => {
     return (
-        <div className="container">
-            <h1>404</h1>
-            <p>Something gone wronge</p>
-        </div>
+        <>
+            <SEO title="Page Not Found" />
+
+            <div className="container">
+                <h1>404</h1>
+                <p>Something gone wronge</p>
+            </div>
+        </>
     );
 };
 
