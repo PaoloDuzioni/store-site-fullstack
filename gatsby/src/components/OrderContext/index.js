55775a08de486369a40725f79048a0e747c6f886
PaoloDuzioni <paolo.duzioni@gmail.com>
2021-01-03 17:24:25 +0100

    fix: code comments

--- gatsby/src/components/OrderContext/index.js
+++ gatsby/src/components/OrderContext/index.js
@@ -2,7 +2,7 @@ import React, { createContext, useState } from 'react';
 const OrderContext = createContext();
 
 export function OrderProvider({ children }) {
-    // Set global state
+    // Set global state, this component wont unmount
     const [order, setOrder] = useState([]);
 
     return (
